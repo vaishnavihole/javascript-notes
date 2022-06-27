e574169b9d9b4827dfa715188a3c722139cf90c5
vaishnavihole <vaishnavihole1@gmail.com>
2022-06-27 10:50:22 +0530

    conditional in js

---
+++ Basics/conditional.js
@@ -0,0 +1,18 @@
+var temperature;
+
+temperature = 49;
+
+//var result = temperature < 20;
+//console.log(result);
+
+if (temperature < 20) {
+    console.log("it's very cold outside")
+
+}
+
+if (temperature < 30) {
+    console.log("it's moderate  outside")
+
+}else{
+    console.log("it's really HOT outside")
+}
