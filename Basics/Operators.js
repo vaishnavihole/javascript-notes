da93a3690990f8574ccaedb2a5287eca6bbdbd19
vaishnavihole <vaishnavihole1@gmail.com>
2022-06-27 09:06:19 +0530

    operators in js

---
+++ Basics/Operators.js
@@ -0,0 +1,24 @@
+var num1 = 7;
+var num2 = 6 
+console.log(num1 + num2);
+console.log(num1 * num2);
+
+
+var answer = num1 > num2;
+
+console.log(answer);
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent =((listingPrice - sellingPrice)/listingPrice) * 100;
+
+console.log("Discount percentage is:" + discountPercent);
+
+displayDiscountPercent = Math.round(discountPercent);
+
+console.log(displayDiscountPercent + "% off");
+
+
+
+
