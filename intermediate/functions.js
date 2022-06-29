8886cf0f78f567371af53d1ee34aa5be41157b81
vaishnavihole <vaishnavihole1@gmail.com>
2022-06-30 00:20:22 +0530

    functions

---
+++ intermediate/functions.js
@@ -0,0 +1,17 @@
+function sayHello(name){
+    console.log("Hello there,Vaishnavi");
+    console.log(`Hello there, ", ${name}. How are you?`);
+}
+
+//sayHello("Vaishu");
+//sayHello("Vaishnavi");
+
+
+function namstey() {
+    return "Hello in India";
+}
+
+var greetings = namstey();
+
+console.log(greetings);
+console,log(namstey());
