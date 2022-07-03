230d3071aea7ac2622f381184aab4af43dbfc811
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-03 09:57:48 +0530

    array

---
+++ intermediate/array.js
@@ -0,0 +1,25 @@
+var countries = ["India", "USA", "Japan","Russia"];
+
+var states = new Array("Rajasthan","Delhi","Mumbai","Assam")
+
+console.log(states[1])
+
+//console.log(states.lenght);
+//states[0] = "Punjab";
+//console.log(states);
+
+var user = ["vaishnavi", "vaishnavi@", 4,45,true];
+console.log(user);
+
+user.pop();
+//console.log(user);
+user.unshift("NEW VALUE");
+//console.log(user);
+user.shift();
+//console.log(user);
+
+console.log(user.indexOf("newyy"));
+
+
+console.log(Array.from("vaishnavi"));
+
