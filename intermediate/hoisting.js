a73c1c77aaa3208a0917b4dcea3e998f46813ead
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-03 07:21:41 +0530

    hoisting

---
+++ intermediate/hoisting.js
@@ -0,0 +1,19 @@
+tipper("80");
+function tipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 5);
+
+}
+
+     
+    var bigTipper  = function(a){
+        var bill = parseInt(a);
+        console.log(bill + 15);
+    
+    };
+
+    bigTipper("200");
+         
+    
+
+    
