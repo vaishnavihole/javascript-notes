68524db2d4d5766c0078b6917003fd28b94c1c99
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-03 12:00:04 +0530

    filterarray

---
+++ intermediate/filterarray.js
@@ -0,0 +1,9 @@
+var testArray = [2,4,6,7,8,9] ;
+
+//console.log(testArray.fill("empty"));
+
+const myNumber = [40,50,70.80,90]
+
+const result = myNumber.filter((num) =>num!= 55);
+
+console,log(result);
