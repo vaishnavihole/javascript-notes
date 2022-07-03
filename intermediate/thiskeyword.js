3987498d34e6163b8c9c75a95b6be33903ef74ba
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-03 08:38:35 +0530

    thiskeyword

---
+++ intermediate/thiskeyword.js
@@ -0,0 +1,20 @@
+//TODO: part 1 for this keyword
+
+console.log(this); 
+
+var game = "cricket";
+
+//function sayName() {
+  //  var name = "Vaishnavi";
+  //  console.log(this);
+//}
+
+//sayName();
+
+
+
+
+
+
+
+
