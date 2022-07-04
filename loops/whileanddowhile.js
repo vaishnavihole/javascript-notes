54fdef0716f6f39a80984720afafb760556448f3
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-04 15:43:15 +0530

    while and dowhile

---
+++ loops/whileanddowhile.js
@@ -0,0 +1,24 @@
+
+
+const myStates =
+ ["Pune",
+"Mumbai",
+1990,
+"Nagar",
+"Shirdi",
+];
+
+//let i = 0;
+//while(i < myStates.lenght) {
+   // i++;
+    
+//}
+
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+}
+while(i < 10);
+
