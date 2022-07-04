561b0fdeeaa99d1f50c417eb0df4a5975f6b64a0
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-04 16:05:58 +0530

    thisagain.js

---
+++ intermediate/thisagain.js
@@ -0,0 +1,20 @@
+console.log(this);
+
+var user = {
+    firstName: "Vaishnavi",
+    courseCount: 4,
+    getCourseCount: function(){
+        console.log("LINE 7", this);
+        function sayHello(){
+            console.log("Hello");
+            console.log("LINE 10",this);
+
+        }
+        sayHello()
+
+    },
+};
+
+user.getCourseCount();
+
+sayHello()
