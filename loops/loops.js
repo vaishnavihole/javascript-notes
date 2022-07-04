87e7aa2a653e884ff4f9045bd2416f619071fd5f
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-04 07:13:13 +0530

    loops

---
+++ loops/loops.js
@@ -0,0 +1,15 @@
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+const myStates =
+ ["Pune",
+"Mumbai",
+1990,
+"Nagar",
+"Shirdi",
+];
+
+for (let i = 0; i <= myStates.length; i++) {
+    console.log(myStates[i]);
+}
