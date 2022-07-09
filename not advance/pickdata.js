e9cd945b89ad0942209abcbca0f7ef25ee649e21
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-09 13:56:41 +0530

    destructure the datacv

---
+++ not advance/pickdata.js
@@ -0,0 +1,18 @@
+//const user = ["vaishnavi",3,"admin"];
+//var role = user[2];
+//var name = user[0];
+
+
+//var [name,courseCount,role] = user;
+//console.log(role);
+
+const myUser = {
+    name:"vaishnavi",
+    courseCount: 5,
+    role: "admin",
+};
+
+console.log(myUser,courseCount);
+
+const {name, courseCount,role } = myUser;
+console.log(role);
